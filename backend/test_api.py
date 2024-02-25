f0e6c8a363ad5f3900bd129cfd79b6e0c5650777
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-02-25 10:41:45 +0300

    Log in test

--- backend/test_api.py
+++ backend/test_api.py
@@ -22,6 +22,19 @@ class APITestCase(unittest.TestCase):
         json=hello_response.json
         
         self.assertEqual(json,{"message": "Hello world"})
+
+    def test_signup(self):
+        signup_response=self.client.post('/auth/signup',
+            json={"username":"testuser",
+            "email":"testuser@test.com",
+            "password":"password"}
+        )
+
+        status_code=signup.response.status_code
+        self.assertEqual(status_code, 201)
+
+    def test_login()
+
     def tearDown(self):
         with self.app.app_context():
             db.session.remove()
