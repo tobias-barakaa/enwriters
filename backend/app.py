3b3a1092fbf8bf2b245a58dc7dcda788ec99367d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-02-25 17:17:01 +0300

    Index.js

--- backend/app.py
+++ backend/app.py
@@ -8,11 +8,14 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from articles import article_ns
 from auth import auth_ns
+from flask_cors import CORS
 
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
+
+    CORS(app)
 
     db.init_app(app)
 
