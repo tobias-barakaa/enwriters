457942082a119254e46d0fe8121c2c296275e052
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-02-26 11:41:31 +0300

    First api call to the backend

---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => {
+
+    useEffect(
+        () => {
+            fetch("/article/hello")
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    setMessage(data.message);
+                })
+                .catch(err => console.log(err));
+        }, []
+    );
+
+    const [message, setMessage] = useState('');
+
+    return (
+        <div className='app'>
+            {message}
+        </div>
+    );
+};
+ReactDOM.render(<App />, document.getElementById('root'))
