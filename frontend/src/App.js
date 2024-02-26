fbe72671933b4b970c96272c33398de90a955f36
tobias-barakaa <ngairato@gmail.com>
2024-02-26 17:28:09 +0300

    yooh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 // import { Form } from "react-router-dom";
 import FormInput from "./components/FormInput";
+import HomeLayout from "./components/HomeLayout";
 import AddArticle from "./pages/AddArticle";
 
 // import AddArticle from './pages/AddArticle';
@@ -7,7 +8,12 @@ import AddArticle from "./pages/AddArticle";
 
 function App() {
   return (
+    <div>
+
+<HomeLayout />
     <AddArticle />
+    </div>
+    
   );
 }
 // tanstackquery
