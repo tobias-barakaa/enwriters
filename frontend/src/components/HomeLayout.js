601745230d779aa350a806c3eeeaab5536e9dc90
tobias-barakaa <ngairato@gmail.com>
2024-02-27 20:48:32 +0300

    folder structuring my code

--- frontend/src/components/HomeLayout.js
+++ frontend/src/components/HomeLayout.js
@@ -1,92 +1,29 @@
 import React from 'react';
 import './HomeLayout.css'
 import { FaSearch, FaFacebook } from 'react-icons/fa'
-import enwriters from '../assets/enwriters.png';
 import AddArticle from '../pages/AddArticle';
 import bg from '../assets/bg.png';
 import backend from '../assets/backend.jpg';
-import soul from '../assets/soul.png';
-import manual from '../assets/manual.jpg';
 import SampleLayout from './SampleLayout';
+import Navigation from './Navigation';
+import HeaderContainer from './HeaderContainer';
 
 
 const HomeLayout = () => {
   return (
     <div>
 
+   <Navigation />
    
-    <nav className="navbar navbar-expand-lg navbar-light bg-blue-tint shadow" style={{ height: '100px' }}>
-  <div className="container-fluid">
-    <a className="navbar-brand text-black fs-4" href="/enwriters">
-      <img src={enwriters} alt="enwriters" width="200" height="200" className="" />
-    </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link text-black" aria-current="page" href="/what">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-black" href="/hey">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-black" href="/contacts">Contacts</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-black" href="/about">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-black" href="/services">Services</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    
 
-<div>
 
-<div className="header-container">
-  <div className="header-text">
-    <h1>Your Company Name</h1>
-    <p>Providing top-notch digital marketing and article writing services</p>
-    <div className="button-group">
-      <button className="btn btn-blue">Get Started</button>
-      <button className="btn btn-dark-blue">Learn More</button>
-      <button className="btn btn-light-blue">View Samples</button>
-    </div>
-  </div>
-</div>
-</div>
+<HeaderContainer />
 
 
 
 
-<div class="sample-articles">Sample Articles</div>
-<div class="samplee">
-    <div class="left">
-        <img src={soul} alt='backend' />
-        <div>
-            <p class="neon-text">AI-generated</p>
-            <div class="separator"></div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-        </div>
-    </div>
-    <div class="right">
-    <img src={manual} alt="Right Image" />
-
-        <div>
-            <p class="neon-text">Manual written</p>
-            <div class="separator"></div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-        </div>
-    </div>
-</div>
+<SampleLayout />
 
 
 
@@ -107,7 +44,7 @@ const HomeLayout = () => {
   </div>
 
 
-    <img  className="bg" src={bg} alt="image"  />
+    <img  className="bg" src={bg} alt="Background"  />
   </div>
 
   
--- frontend/src/components/SampleLayout.js
+++ frontend/src/components/SampleLayout.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './SampleLayout.css'
+import soul from '../assets/soul.png';
+import manual from '../assets/manual.jpg';
 
 const SampleLayout = () => {
   return (
     <div>
-   <div class="samplee">
+   <div class="sample-articles">Sample Articles</div>
+<div class="samplee">
     <div class="left">
-        <p class="text">AI-generated</p>
+        <img src={soul} alt='backend' />
+        <div>
+            <p class="neon-text">AI-generated</p>
+            <div class="separator"></div>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
     </div>
-    
+    <div class="right">
+    <img src={manual} alt="Manual written article" />
+
+        <div>
+            <p class="neon-text">Manual written</p>
+            <div class="separator"></div>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+    </div>
 </div>
      
     </div>
